f15aafc4c1f60c4e0eb2083ee6fe8e1af30a8578
Will-Andrade <willian.santos.andra@outlook.com>
2025-07-21 15:10:19 -0400

    feat: start project

---
+++ src/test/java/com/andradev/scanify/ScanifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andradev.scanify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
